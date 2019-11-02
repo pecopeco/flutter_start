dec78c5ea97b5745f0fe9091b18c0fb7edac62e4
pecopeco <yangpeco@qq.com>
2019-11-02 16:41:19 +0800

    添加路由跳转、图片、引入依赖包

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,10 +8,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      initialRoute:"/",
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      routes:{
+        "/":(context)=> MyHomePage(title: 'Flutter Demo Home Page'),
+        "new_route":(context)=>NewRoute(),
+        "tip_route":(context)=>TipRoute(text: ModalRoute.of(context).settings.arguments),
+      },
     );
   }
 }
@@ -50,6 +56,25 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.display1,
             ),
+            FlatButton(
+              child: Text("open new route"),
+              textColor: Colors.blue,
+              onPressed: () {
+                //导航到新路由   
+                Navigator.pushNamed(context, "new_route");
+              },
+            ),
+            RaisedButton(
+              onPressed: () async {
+                // 打开tip_route，传递参数并等待返回结果
+                var result = await Navigator.of(context).pushNamed("tip_route", arguments: "553");
+                // 输出tip_route路由返回结果
+                print("路由返回值: $result");
+              },
+              child: Text("打开提示页"),
+            ),
+            RandomWordsWidget(),
+            Image.asset("assets/logo.png"),
           ],
         ),
       ),
@@ -57,6 +82,68 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+// 随机字符串
+class RandomWordsWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+   // 生成随机字符串
+    final wordPair = new WordPair.random();
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: new Text("随机字符：" + wordPair.toString()),
+    );
+  }
+}
+
+// 路由跳转测试页
+class NewRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("New route"),
+      ),
+      body: Center(
+        child: Text("This is new route"),
+      ),
+    );
+  }
+}
+
+// 路由传参测试页
+class TipRoute extends StatelessWidget {
+  // 接收一个text参数
+  TipRoute({
+    Key key,
+    @required this.text,
+  }) : super(key: key);
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("提示"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(18),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text("接收参数：" + text),
+              RaisedButton(
+                // 返回并传递一个参数
+                onPressed: () => Navigator.pop(context, "233"),
+                child: Text("返回"),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
